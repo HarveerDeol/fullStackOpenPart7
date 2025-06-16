e35a8c4d166a7cece96ad8897c868c8190753ab1
Harveer Deol <harveerdeol53@gmail.com>
2025-06-16 13:02:28 -0600

    completed excerise 7.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import  { useField } from './hooks'
 import {BrowserRouter as Router, Routes, Route, Link, useParams, useNavigate} from 'react-router-dom'
 
 const Anecdote = ({ anecdotes }) => {
@@ -48,18 +49,20 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+  
   const navigate = useNavigate()
+  //const username = useField('text')
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      infor : info.value,
       votes: 0
     })
     navigate('/AnecdoteList')
@@ -71,15 +74,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
